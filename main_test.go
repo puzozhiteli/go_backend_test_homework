41b9547051cfd3c7138ba7a2ff8dbd94495cf2da
Maxim Nechaev <max.nech131@mail.ru>
2024-12-15 14:23:13 +0300

    Add first commit

--- main_test.go
+++ main_test.go
@@ -13,7 +13,7 @@ func TestCheckRequiredFiles(t *testing.T) {
 
 	dir, err := os.Getwd()
 	if err != nil {
-		t.Errorf(err.Error())
+		t.Errorf("%s", err.Error())
 	}
 
 	files, err := os.ReadDir(dir)
